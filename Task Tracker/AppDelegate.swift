292507696f8adfe2f1bef8c873766286a1f60635
Erik Beerepoot <erikbeerepoot@hush.com>
2015-10-26 20:45:02 -0700

    Initial Commit

---
+++ Task Tracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Task Tracker
+//
+//  Created by Erik Beerepoot on 2015-10-26.
+//  Copyright © 2015 Barefoot Systems. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Task Tracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Task Tracker
+//
+//  Created by Erik Beerepoot on 2015-10-26.
+//  Copyright © 2015 Barefoot Systems. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
