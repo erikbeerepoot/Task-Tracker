f4c0e1a6d639348142c6f9672fbfd9d27417bbda
Erik Beerepoot <erikbeerepoot@hush.com>
2016-01-01 15:34:49 -0500

    Finished new coloured button code. Consists of two CALayers for the outline and the background, as well as a transparent NSTextField to display the button's text.

--- Task Tracker/EEBBorderedColourButton.swift
+++ Task Tracker/EEBBorderedColourButton.swift
@@ -14,44 +14,102 @@ class EEBBorderedColourButton : NSButton {
     let kCornerRadius : CGFloat = 3.0
     let kBorderThickness : CGFloat = 0.25
     
+    let backgroundLayer = CALayer()
+    let outlineLayer = CALayer()
+    var textfield : NSTextField? = nil
+    
     var borderColor =  NSColor(calibratedRed:0.816,green:0.007,blue:0.106,alpha:1.0) {
         didSet {
-            self.layer?.borderColor = borderColor.CGColor
+            outlineLayer.backgroundColor = borderColor.CGColor
+            outlineLayer.setNeedsDisplay()
         }
     }
     var backgroundColor =  NSColor(calibratedRed:0.988,green:0.835,blue:0.859,alpha:1.0) {
         didSet {
-            self.layer?.backgroundColor = backgroundColor.CGColor
+            backgroundLayer.backgroundColor = backgroundColor.CGColor
+            backgroundLayer.setNeedsDisplay()
+        }
+    }
+    
+    var text : String = "" {
+        didSet {
+            textfield?.stringValue = text
         }
     }
     
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
+        bounds = frame
         
         //turn off default "border"
         self.bordered = false;
+        self.wantsLayer = true
         
         //draw custom border & transparent background
-        self.layer = CALayer()
-        self.layer?.borderColor = borderColor.CGColor
-        self.layer?.backgroundColor = backgroundColor.CGColor
-        self.layer?.borderWidth = kBorderThickness
-        self.layer?.cornerRadius = kCornerRadius
+        outlineLayer.frame = frameRect
+        outlineLayer.backgroundColor = borderColor.CGColor
+        outlineLayer.cornerRadius = kCornerRadius
+
+        backgroundLayer.frame.origin.x = frameRect.origin.x + 0.5
+        backgroundLayer.frame.origin.y = frameRect.origin.y + 0.5
+        backgroundLayer.frame.size.width = frameRect.size.width - 1
+        backgroundLayer.frame.size.height = frameRect.size.height - 1
+        backgroundLayer.backgroundColor = NSColor.clearColor().CGColor
+        backgroundLayer.cornerRadius = kCornerRadius
+        
+        textfield = NSTextField(frame: frameRect)
+        textfield!.frame.origin.y -= 2
+        textfield!.bezeled = false
+        textfield!.editable = false
+        textfield!.drawsBackground = false
+        textfield!.alignment = .Center
+        textfield!.font = NSFont(name: "Helvetica Neue Light", size: 12.0)
+        textfield!.textColor = NSColor.darkGrayColor()
+        
+        addSubview(textfield!)
+        layer?.addSublayer(outlineLayer)
+        layer?.addSublayer(backgroundLayer)
+        layer?.cornerRadius = kCornerRadius
     }
     
     required init?(coder: NSCoder) {
+        super.init(coder:coder)
         
-        super.init(coder: coder)
+        bounds = frame
         
         //turn off default "border"
         self.bordered = false;
+        self.wantsLayer = true
         
         //draw custom border & transparent background
-        self.layer = CALayer()
-        self.layer?.borderColor = borderColor.CGColor
-        self.layer?.backgroundColor = backgroundColor.CGColor
-        self.layer?.borderWidth = kBorderThickness
-        self.layer?.cornerRadius = kCornerRadius
+        outlineLayer.frame = frame
+        outlineLayer.backgroundColor = borderColor.CGColor
+        outlineLayer.cornerRadius = kCornerRadius
+        
+        backgroundLayer.frame.origin.x = frame.origin.x + 0.5
+        backgroundLayer.frame.origin.y = frame.origin.y + 0.5
+        backgroundLayer.frame.size.width = frame.size.width - 1
+        backgroundLayer.frame.size.height = frame.size.height - 1
+        backgroundLayer.backgroundColor = backgroundColor.CGColor
+        backgroundLayer.cornerRadius = kCornerRadius
+        
+        textfield = NSTextField(frame: frame)
+        textfield!.frame.origin.y -= 2
+        textfield!.bezeled = false
+        textfield!.editable = false
+        textfield!.drawsBackground = false
+        textfield!.alignment = .Center
+        textfield!.font = NSFont(name: "Helvetica Neue Light", size: 12.0)
+        textfield!.textColor = NSColor.darkGrayColor()
+        
+        addSubview(textfield!)
+        layer?.addSublayer(outlineLayer)
+        layer?.addSublayer(backgroundLayer)
+        layer?.cornerRadius = kCornerRadius
+        
+        layer?.addSublayer(outlineLayer)
+        layer?.addSublayer(backgroundLayer)
+        layer?.cornerRadius = kCornerRadius
     }
     
 }
