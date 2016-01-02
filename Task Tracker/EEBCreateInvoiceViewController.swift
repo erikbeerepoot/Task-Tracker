52428a43ce7942158f18c819efbca53c14dc7d20
Erik Beerepoot <erikbeerepoot@hush.com>
2016-01-02 14:21:13 -0500

    Added basics of viewcontroller used to create new invoices

--- Task Tracker/EEBCreateInvoiceViewController.swift
+++ Task Tracker/EEBCreateInvoiceViewController.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import AppKit
+
+class EEBCreateInvoiceViewController : NSViewController, NavigableViewController {
+    
+    /**** Top level views ****/
+    @IBOutlet weak var overlayView : EEBOverlayView!
+    @IBOutlet weak var customSpacerView : NSView!
+    @IBOutlet weak var backgroundView : NSView!
+
+    /**** Create subview ****/
+    @IBOutlet weak var createOptionsView : NSView!
+    
+    //constraint selection
+    @IBOutlet weak var chkbtn_selection : NSButton!
+    @IBOutlet weak var chkbtn_dataRange : NSButton!
+    @IBOutlet weak var chkbtn_expression : NSButton!
+
+    //constraint details
+    @IBOutlet weak var fromDatePicker : NSDatePicker!
+    @IBOutlet weak var toDatePicker : NSDatePicker!
+    @IBOutlet weak var expressionField : NSTextField!
+    
+    //action buttons
+    @IBOutlet weak var openButton : NSButton!
+    @IBOutlet weak var createButton : NSButton!
+
+    
+    var navigationController : EEBNavigationController? = nil;
+    var storeManager : EEBPersistentStoreManager? = nil
+        
+    override func viewDidLoad(){
+        print("Create new invoice")
+    }
+    
+    @IBAction func create(sender : AnyObject){
+        
+    }
+
+    @IBAction func open(sender : AnyObject){
+        
+    }
+}
