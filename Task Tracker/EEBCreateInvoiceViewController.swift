a8876ac0571f760161ef433e0d6ddf6c159d86d2
Erik Beerepoot <erikbeerepoot@hush.com>
2016-01-02 14:19:45 -0500

    Added new viewcontroller in storyboard to handle invoice creation

---
+++ Task Tracker/EEBCreateInvoiceViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EEBCreateInvoiceViewController.swift
+//  Task Tracker
+//
+//  Created by Erik Beerepoot on 2016-01-02.
+//  Copyright © 2016 Barefoot Systems. All rights reserved.
+//
+
+import Foundation
